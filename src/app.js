97587d79538d6b524b7defa203863db7fd58c376
Sean Behan <codebam@riseup.net>
2019-03-21 01:35:29 -0400

    Refactor
    changed to use proper asyncronousness :)

--- src/app.js
+++ src/app.js
@@ -1,35 +1,25 @@
 'use strict'
 
-const https = require('https')
 const TelegramBot = require('node-telegram-bot-api')
+const fetch = require('node-fetch')
 
 const token = require('./token.js').token
 const bot = new TelegramBot(token, { polling: true })
 
-bot.on('inline_query', (msg) =>
-  https.get('https://api.kanye.rest', (resp) => {
-    console.log(msg)
-    let data = ''
-    resp.on('data', (chunk) => {
-      data += chunk
-    })
-    resp.on('end', () => {
-      let quote = JSON.parse(data).quote
-      console.log(quote)
-      console.log(msg)
-      bot.answerInlineQuery(msg.id, JSON.stringify([{
-        type: 'article',
-        id: 0,
-        title: 'Kanye says...',
-        input_message_content: {
-          message_text: quote
-        },
-        description: quote
-      }]), {
-        cache_time: 0
-      })
-    })
-  }).on('error', (err) => {
-    console.log('Error: ' + err.message)
-  })
+const sendToTelegram = (msg, quote) => bot.answerInlineQuery(msg.id, JSON.stringify([{
+  type: 'article',
+  id: 0,
+  title: 'Kanye says...',
+  input_message_content: {
+    message_text: quote
+  },
+  description: quote
+}]), {
+  cache_time: 0
+})
+
+bot.on('inline_query', (msg) => fetch('https://api.kanye.rest')
+  .then(resp => resp.json())
+  .then(json => json.quote)
+  .then(quote => sendToTelegram(msg, quote))
 )
