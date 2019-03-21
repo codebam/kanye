78cedadad65c310a525a0535904668237308c85d
Sean Behan <codebam@riseup.net>
2019-03-21 10:34:07 -0400

    Cleanup sendToTelegram

--- src/app.js
+++ src/app.js
@@ -6,19 +6,17 @@ const fetch = require('node-fetch')
 const token = require('./token.js').token
 const bot = new Telegraf(token)
 
-const sendToTelegram = (bot, id, quote) => {
-  bot.telegram.answerInlineQuery(id, [{
-    type: 'article',
-    id: 0,
-    title: 'Kanye says...',
-    input_message_content: {
-      message_text: quote
-    },
-    description: quote
-  }], {
-    cache_time: 0
-  })
-}
+const sendToTelegram = (bot, id, quote) => bot.telegram.answerInlineQuery(id, [{
+  type: 'article',
+  id: 0,
+  title: 'Kanye says...',
+  input_message_content: {
+    message_text: quote
+  },
+  description: quote
+}], {
+  cache_time: 0
+})
 
 bot.on('inline_query', (ctx) => fetch('https://api.kanye.rest')
   .then(resp => resp.json())
