8cb2a2c8b3d3e418c6cdb7988b9dae5d62eca9d2
Sean Behan <codebam@riseup.net>
2019-03-21 14:22:05 -0400

    Lint according to airbnb

--- src/app.js
+++ src/app.js
@@ -1,27 +1,24 @@
-'use strict'
+const Telegraf = require('telegraf');
+const fetch = require('node-fetch');
+const { token } = require('./token.js');
 
-const Telegraf = require('telegraf')
-const fetch = require('node-fetch')
+const bot = new Telegraf(token);
 
-const token = require('./token.js').token
-const bot = new Telegraf(token)
-
-const sendToTelegram = (bot, id, quote) => bot.telegram.answerInlineQuery(id, [{
+const sendToTelegram = (b, id, quote) => b.telegram.answerInlineQuery(id, [{
   type: 'article',
   id: 0,
   title: 'Kanye says...',
   input_message_content: {
-    message_text: quote
+    message_text: quote,
   },
-  description: quote
+  description: quote,
 }], {
-  cache_time: 0
-})
+  cache_time: 0,
+});
 
-bot.on('inline_query', (ctx) => fetch('https://api.kanye.rest')
+bot.on('inline_query', ctx => fetch('https://api.kanye.rest')
   .then(resp => resp.json())
   .then(json => json.quote)
-  .then(quote => sendToTelegram(bot, ctx.update.inline_query.id, quote))
-)
+  .then(quote => sendToTelegram(bot, ctx.update.inline_query.id, quote)));
 
-bot.launch()
+bot.launch();
