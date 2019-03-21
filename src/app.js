6a897591ccfd3d4b3bc8226bfe48041b6b1d6a50
Sean Behan <codebam@riseup.net>
2019-03-21 10:05:40 -0400

    Migrate to Telegraf framework

--- src/app.js
+++ src/app.js
@@ -1,25 +1,29 @@
 'use strict'
 
-const TelegramBot = require('node-telegram-bot-api')
+const Telegraf = require('telegraf')
 const fetch = require('node-fetch')
 
 const token = require('./token.js').token
-const bot = new TelegramBot(token, { polling: true })
+const bot = new Telegraf(token)
 
-const sendToTelegram = (msg, quote) => bot.answerInlineQuery(msg.id, JSON.stringify([{
-  type: 'article',
-  id: 0,
-  title: 'Kanye says...',
-  input_message_content: {
-    message_text: quote
-  },
-  description: quote
-}]), {
-  cache_time: 0
-})
+const sendToTelegram = (bot, id, quote) => {
+  bot.telegram.answerInlineQuery(id, [{
+    type: 'article',
+    id: 0,
+    title: 'Kanye says...',
+    input_message_content: {
+      message_text: quote
+    },
+    description: quote
+  }], {
+    cache_time: 0
+  })
+}
 
-bot.on('inline_query', (msg) => fetch('https://api.kanye.rest')
+bot.on('inline_query', (ctx) => fetch('https://api.kanye.rest')
   .then(resp => resp.json())
   .then(json => json.quote)
-  .then(quote => sendToTelegram(msg, quote))
+  .then(quote => sendToTelegram(bot, ctx.update.inline_query.id, quote))
 )
+
+bot.launch()
